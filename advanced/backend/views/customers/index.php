9c840ef71e041a81ee498db9121f34b7a2dbaf47
rakeshpatil919 <rakeshpatil919@gmail.com>
2018-03-02 22:31:46 +0530

    customers listing
    customers listing

--- advanced/backend/views/customers/index.php
+++ advanced/backend/views/customers/index.php
@@ -26,7 +26,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            'unique_id',
             'username',
             'first_name',
             'last_name',
